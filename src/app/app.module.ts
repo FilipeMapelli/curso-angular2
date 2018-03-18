73f24d2542692e541d5b606c48c3cd45296bf57c
Filipe Mapelli <filipemapellis@gmail.com>
2018-03-18 12:02:02 -0300

    Input Properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
 import { CursosModule } from './cursos/cursos.module';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { InputPropertyComponent } from './input-property/input-property.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
     MeuPrimeiroComponent,
     MeuPrimeiro2Component,
     DataBindingComponent,
+    InputPropertyComponent,
   ],
   imports: [
     BrowserModule,
